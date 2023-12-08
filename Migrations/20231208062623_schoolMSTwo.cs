bc22e2ba41960a8b72cf326d76b28f7579315fd3
samuelkamotho92 <samuelkamotho92@gmail.com>
2023-12-08 09:39:07 +0300

    Started working on Departments

---
+++ Migrations/20231208062623_schoolMSTwo.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCoreCodeFirstDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class schoolMSTwo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "departmentName",
+                table: "Departments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "departmentName",
+                table: "Departments");
+        }
+    }
+}
--- Services/DepartmentServ/DepartmentServ.cs
+++ Services/DepartmentServ/DepartmentServ.cs
@@ -9,8 +9,44 @@ namespace EFCoreCodeFirstDemo.Services.DepartmentServ
 {
     public class DepartmentServ : IDepartment
     {
+        SMSCoreDbContext smsDbContext = new SMSCoreDbContext();
+
+        public int userInput()
+        {
+
+            Console.WriteLine("Enter The Department Id You want To View");
+            string userInpt = Console.ReadLine();
+            int num;
+            bool stateInput = int.TryParse(userInpt, out num);
+            if (stateInput)
+            {
+               List<Department> departmentList = new List<Department>();
+                if (num <= departmentList.Count)
+                {
+                    return num;
+                }
+                else
+                {
+                    Console.WriteLine("Department with Id does not exist yet");
+                    userInput();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Enter a number please");
+            }
+            return num;
+        }
         public async Task<Department> AddDepartment()
         {
+            Department department = new Department();
+            Console.WriteLine("Entert Department Name");
+            string departmentName = Console.ReadLine();
+            department.departmentName = departmentName;
+            smsDbContext.Add(department);
+            smsDbContext.SaveChanges();
+           
+
            // Console.WriteLine("Enter Department");
             throw new NotImplementedException();
 
